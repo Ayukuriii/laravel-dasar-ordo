dd30bbfc36548ecf48c8f5fc314aefe56e03c5a0
Ekadian Haris <ekadianharis@gmail.com>
2024-02-06 09:18:05 +0700

    Book: Detail

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -34,4 +34,11 @@ class BookController extends Controller
 
         return to_route('book.index')->with('success', 'Book created successfully');
     }
+
+    public function show(string $id)
+    {
+        $book = Book::find($id);
+
+        return view('books.show', compact('book'));
+    }
 }
