5a0c176893fc0f30f9a1445f4579c23be49747b4
Ekadian Haris <ekadianharis@gmail.com>
2024-02-13 13:22:31 +0700

    Book: Create

--- app/Http/Controllers/Api/BookController.php
+++ app/Http/Controllers/Api/BookController.php
@@ -13,4 +13,20 @@ class BookController extends Controller
         $books = Book::paginate();
         return response()->json($books);
     }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'cover_image' => ['required', 'image', 'mimes:png,jpg,jpeg,gif,svg', 'max:2048'],
+            'name' => ['required', 'string', 'max:255'],
+            'author' => ['required', 'string', 'max:255'],
+            'description' => ['required', 'string'],
+            'is_published' => ['required', 'boolean']
+        ]);
+
+        $validatedData['cover_image'] = $request->file('cover_image')->store('image', 'public');
+        Book::create($validatedData);
+
+        return response()->json(['success' => 'Book created successfully']);
+    }
 }
